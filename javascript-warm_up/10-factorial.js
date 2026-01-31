4181fa601738c20f4326e0d86c0263c28d833088
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-01-31 02:12:50 +0200

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,18 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const fact = parseInt(process.argv.slice(2)[0]);
+
+if (isNaN(fact) === true) {
+  console.log(1);
+} else {
+  const result = Factorial(fact);
+  console.log(result);
+}
+
+function Factorial (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return (num * Factorial(num - 1));
+  }
+}
